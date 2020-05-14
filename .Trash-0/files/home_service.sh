28696373963f7c453ea263cb0e8f7d5b6da7ddb5
Sumit Chopra <sumitch@umich.edu>
2020-05-14 04:37:34 +0000

    Initial Commit

---
+++ .Trash-0/files/home_service.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/map/sumitwithball.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/map/map_v1.yaml
+
+xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch  " &
+sleep 5
+xterm -e " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm -e " source devel/setup.bash; rosrun pick_objects pick_objects " &
+sleep 5
+xterm -e " source devel/setup.bash; rosrun add_markers add_markers "
